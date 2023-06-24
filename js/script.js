3d525b8a2d6a9c8a9a44f99daa8b098f997e575b
yeongyu05 <89061456+yeongyu05@users.noreply.github.com>
2023-06-24 17:59:04 +0900

    init

---
+++ js/script.js
@@ -0,0 +1,476 @@
+const questions = {
+    'html': [
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+    ],
+    'css': [
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+    ],
+    'js': [
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+    ],
+    'php': [
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+    ],
+    'mysql': [
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+        {
+            question: 'question1',
+            answers: [
+                {answer: 'answer1', isCorrect: true},
+                {answer: 'answer2', isCorrect: false},
+                {answer: 'answer3', isCorrect: false},
+                {answer: 'answer4', isCorrect: false},
+            ]
+        },
+    ],
+}
+
+const data = [
+    {label: 'Category 1', value: 20, color: '#f00'},
+    {label: 'Category 2', value: 10, color: '#f80'},
+    {label: 'Category 3', value: 40, color: '#ff0'},
+    {label: 'Category 4', value: 30, color: '#0f0'},
+    {label: 'Category 5', value: 50, color: '#00f'},
+];
+
+const state = 'html';
+
+const renderQuestions = () => {
+    const container = document.querySelector('.container');
+    const data = questions[state];
+    data.forEach(({question, answers}) => {
+        const item = document.createElement('div');
+        const q = document.createElement('div');
+        const as = document.createElement('div');
+        item.className = 'item';
+        q.className = 'question';
+        as.className = 'answers';
+        answers.forEach((answer, isCorrect) => {
+            const a = document.createElement('div');
+            a.className = 'answer';
+            a.textContent = answer;
+        });
+    });
+}
+
+const renderBarGraph = (canvasId, data) => {
+    // canvas Setting
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    const canvasWidth = 1080;
+    const canvasHeight = 720;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    
+    const getMaxValue = () => {
+        let maxValue = -Infinity;
+        for (const {value} of data) {
+            value > maxValue ? maxValue = value : maxValue;
+        }
+        return maxValue;
+    }
+    
+    // draw rows
+    const [pt, pb, pl, pr] = [50, 50, 50, 50];
+    const maxValue = getMaxValue(); // test;
+    const maxHeight = canvasHeight - pt - pb;
+    const rowGap = 10;
+    const rowCount = maxValue / rowGap;
+    const rowLimit = maxHeight / rowCount;
+
+    for(let i = 0; i <= rowCount; i++) {
+        const y = canvasHeight - pb - i * rowLimit 
+        ctx.beginPath();
+        ctx.moveTo(pl, y);
+        ctx.lineTo(canvasWidth - pr, y);
+        ctx.stroke();
+
+        ctx.font = '16px sans-serif';
+        ctx.fillText(rowGap * i, pl, y);
+    }
+
+
+    // draw bars
+    const barWidth = 100;
+    data.forEach(({value}, idx) => {
+        const x = barWidth * idx + barWidth * idx;
+        const y = maxHeight - maxHeight / maxValue * value;
+        const width = barWidth;
+        const height = maxHeight / maxValue * value;
+        ctx.fillRect(x + barWidth, y + pb, width, height);
+    });
+}
+
+const renderLineGraph = (canvasId, data) => {
+    // canvas Setting
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    const canvasWidth = 1080;
+    const canvasHeight = 720;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    const getMaxValue = () => {
+        let maxValue = -Infinity;
+        for (const {value} of data) {
+            value > maxValue ? maxValue = value : maxValue;
+        }
+        return maxValue;
+    }
+    
+    // draw rows
+    const [pt, pb, pl, pr] = [50, 50, 50, 50];
+    const maxValue = getMaxValue();
+    const maxHeight = canvasHeight - pt - pb;
+    const rowGap = 10;
+    const rowCount = maxValue / rowGap;
+    const rowLimit = maxHeight / rowCount;
+    for(let i = 0; i <= rowCount; i++) {
+        const y = canvasHeight - pb - i * rowLimit 
+        ctx.beginPath();
+        ctx.moveTo(pl, y);
+        ctx.lineTo(canvasWidth - pr, y);
+        ctx.stroke();
+
+        ctx.font = '16px sans-serif';
+        ctx.fillText(rowGap * i, pl, y);
+    }
+
+    // draw line
+    const maxWidth = canvasWidth - pl - pr;
+    data.forEach(({value}, idx) => {
+        const gap = maxWidth / data.length;
+        const x = canvasWidth - maxWidth + pl + gap * idx;
+        const y = maxHeight - maxHeight / maxValue * value + pb;
+        idx ? ctx.lineTo(x, y) : ctx.moveTo(x, y);
+        ctx.stroke();
+    });
+
+    // draw circle
+    data.forEach(({value}, idx) => {
+        const gap = maxWidth / data.length;
+        const x = canvasWidth - maxWidth + pl + gap * idx;
+        const y = maxHeight - maxHeight / maxValue * value + pb;
+        const radius = 5;
+        const startAngle = 0;
+        const endAngle = Math.PI * 2;
+        ctx.beginPath();
+        ctx.arc(x, y, radius, startAngle, endAngle);
+        ctx.fill();
+    })
+}
+
+const renderPolygonGraph = (canvasId, data) => {
+    // canvas Setting
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    const canvasWidth = 1080;
+    const canvasHeight = 720;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    const getMaxValue = () => {
+        let maxValue = -Infinity;
+        for (const {value} of data) {
+            value > maxValue ? maxValue = value : maxValue;
+        }
+        return maxValue;
+    }
+
+    // draw pentagon
+    const numberOfSides = data.length;
+    const maxSize = 350;
+    const Xcenter = canvasWidth / 2;
+    const Ycenter = canvasHeight / 2;
+    const step = Math.PI * 2 / numberOfSides; // pre calculate step value
+    const shift = (Math.PI / 180.0) * 270; // Quick fix;
+    const maxValue = Math.ceil(getMaxValue() / 10) * 10;
+    const gapCount = maxValue / 10;
+
+    ctx.beginPath();
+    for(let gap = 1; gap <= gapCount; gap++) {
+        const size = maxSize / gapCount * gap;
+        for (let i = 0; i <= numberOfSides; i++) {
+            let curStep = i * step + shift;
+            const x = Xcenter + size * Math.cos(curStep);
+            const y = Ycenter + size * Math.sin(curStep);
+            // i ? ctx.lineTo(x, y) : ctx.moveTo(x, y);
+            ctx.lineTo(x, y);
+        }
+        const Xtext = Xcenter + size * Math.cos(numberOfSides * step + shift);
+        const Ytext = Ycenter + size * Math.sin(numberOfSides * step + shift);
+        ctx.font = '16px sans-serif';
+        ctx.fillText(gap * 10, Xtext + 10, Ytext + 50);
+    }
+    ctx.stroke();
+
+    ctx.beginPath();
+    data.forEach(({value}, idx) => {
+        const size = maxSize / maxValue * value;
+        let curStep = (idx+1) * step + shift;
+        const x = Xcenter + size * Math.cos(curStep);
+        const y = Ycenter + size * Math.sin(curStep);
+        ctx.lineTo(x, y);
+    });
+    ctx.fillStyle = 'rgba(0, 0, 0, .5)';
+    ctx.fill();
+}
+
+const renderPieGraph = (canvasId, data) => {
+    // canvas Setting
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    const canvasWidth = 1080;
+    const canvasHeight = 720;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    const centerX = canvasWidth / 2;
+    const centerY = canvasHeight / 2;
+    const radius = Math.min(centerX, centerY) - 10;
+    let startAngle = 0;
+
+    // Calculate the total value of all data points
+    const total = data.reduce((sum, point) => sum + point.value, 0);
+
+    // Draw each segment of the pie
+    data.forEach(point => {
+        const sliceAngle = (2 * Math.PI * point.value) / total;
+
+        ctx.beginPath();
+        ctx.fillStyle = point.color;
+        ctx.moveTo(centerX, centerY);
+        ctx.arc(centerX, centerY, radius, startAngle, startAngle + sliceAngle);
+        ctx.closePath();
+        ctx.fill();
+
+        // draw the lable
+        const labelX = centerX + (radius / 2) * Math.cos(startAngle + sliceAngle / 2);
+        const labelY = centerY + (radius / 2) * Math.sin(startAngle + sliceAngle / 2);
+        ctx.fillStyle = '#000';
+        ctx.font = '12px Arial';
+        ctx.fillText(point.label, labelX - 25, labelY);
+
+        startAngle += sliceAngle;
+    });
+}
+
+const render = () => {
+    renderBarGraph('bar', data);
+    renderLineGraph('line', data);
+    renderPolygonGraph('polygon', data);
+    renderPieGraph('pie', data);
+}
+
+render();
